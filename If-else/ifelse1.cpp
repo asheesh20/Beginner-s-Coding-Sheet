7101cf5d12a97313cc6a5d6995641268c70218af
Asheesh Kumar <asheesh220720@gmail.com>
2023-02-15 06:40:56 +0530

    Create ifelse1.cpp

---
+++ If-else/ifelse1.cpp
@@ -0,0 +1,23 @@
+// Checking using Modulo Operator
+
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int32_t main(){
+	int n;
+	cin>>n;
+	if(n%2==0) cout<<"Even"<<endl;
+	else cout<<"Odd"<<endl;
+}
+
+// Checking Using & Opeartor
+
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int32_t main(){
+	int n;
+	cin>>n;
+	if(n&1) cout<<"Odd"<<endl;
+	else cout<<"Even"<<endl;
+}
