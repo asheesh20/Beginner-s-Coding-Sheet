473edd742597779759eccfd0dc4ad707abadf562
Asheesh Kumar <asheesh220720@gmail.com>
2023-02-15 06:32:58 +0530

    Add files via upload

---
+++ Basic/basic5.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int32_t main(){
+	cout<<"Size of Char "<<sizeof(char)<<endl;
+	cout<<"Size of Int "<<sizeof(int)<<endl;
+	cout<<"Size of Float "<<sizeof(float)<<endl;
+	cout<<"Size of Double "<<sizeof(double)<<endl;
+	cout<<"Size of Long Int "<<sizeof(long int)<<endl;
+	cout<<"Size of Long Long Int "<<sizeof(long long int)<<endl;
+}
