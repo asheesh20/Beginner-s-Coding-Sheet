a0fbbc908b144cc6f7889b2f32d1926c2185c3c9
Asheesh Kumar <asheesh220720@gmail.com>
2023-02-15 06:42:23 +0530

    Add files via upload

---
+++ If-else/ifelse3.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int32_t main(){
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(a>b && a>c) cout<<"A is Greatest"<<endl;
+	else if(b>a && b>c) cout<<"B is Greatest"<<endl;
+	else cout<<"C is Greatest"<<endl;
+}
