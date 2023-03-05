4863c347c581301a1f6d1ba36a45c9452976f399
Asheesh Kumar <asheesh220720@gmail.com>
2023-03-05 13:48:59 +0530

    Create pattern1.cpp

---
+++ Pattern/pattern1.cpp
@@ -0,0 +1,15 @@
+// Solid Rectangular Star
+
+#include<bits/stdc++.h>
+#define ll long long 
+#define ull unsigned long long 
+using namespace std;
+int32_t main(){
+	for(int i=1;i<=3;i++){
+		for(int j=1;j<=5;j++){
+			if(j<=5) cout<<" *" ;
+			else cout<<" ";
+		}
+		if(i<3) cout<<endl;
+	}
+}
