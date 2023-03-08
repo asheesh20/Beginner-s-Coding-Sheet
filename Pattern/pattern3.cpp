836238134fce40ea5e3e2a76e09f6778d0a47ab2
Asheesh Kumar <asheesh220720@gmail.com>
2023-03-08 14:54:56 +0530

    Add files via upload

---
+++ Pattern/pattern3.cpp
@@ -0,0 +1,15 @@
+// Half Pyramid Star Patter
+
+#include<bits/stdc++.h>
+#define ll long long 
+#define ull unsigned long long 
+using namespace std;
+int32_t main(){
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=5;j++){
+			if(j<=i) cout<<"*";
+		    else cout<<" ";
+	}
+	if(i<5) cout<<endl;
+  }
+}
