8e707dbbcb22fe78730ff7a347bb0f7af0678b86
Asheesh Kumar <asheesh220720@gmail.com>
2023-03-11 01:45:47 +0530

    Add files via upload

---
+++ Pattern/pattern6.cpp
@@ -0,0 +1,40 @@
+// Inverted Full Pyramid
+// Pyramid Type 1
+
+#include<bits/stdc++.h>
+#define ll long long 
+#define ull unsigned long long 
+using namespace std;
+int32_t main(){
+	int k;
+	for(int i=1;i<=6;i++){k=1;
+		for(int j=1;j<=11;j++){
+			if(j>=i && j<=12-i && k){
+				cout<<"*";
+				k=0;
+			}
+			else {
+				cout<<" ";
+				k=1;
+			}
+		}
+		if(i<6) cout<<endl;
+	}
+}
+
+
+// Pyramid Type 2
+
+#include<bits/stdc++.h>
+#define ll long long 
+#define ull unsigned long long 
+using namespace std;
+int32_t main(){
+	for(int i=1;i<=6;i++){
+		for(int j=1;j<=11;j++){
+			if(j>=i && j<=12-i) cout<<"*";
+			else cout<<" ";
+		}
+		if(i<6) cout<<endl;
+	}
+}
