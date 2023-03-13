beb902f7ea3b6ec447ec1a7088c7e2067933081c
Asheesh Kumar <asheesh220720@gmail.com>
2023-03-13 16:21:57 +0530

    Add files via upload

---
+++ Pattern/pattern8.cpp
@@ -0,0 +1,19 @@
+// Half Pyramid using Numbers
+
+#include<bits/stdc++.h>
+#define ll long long 
+#define ull unsigned long long 
+using namespace std;
+int32_t main(){
+	int k;
+	for(int i=1;i<=5;i++){k=1;
+		for(int j=1;j<=5;j++){
+			if(j<=i){
+				cout<<k<<" ";
+				k++;
+			}
+			else cout<<" ";
+		}
+		if(i<5) cout<<endl;
+	}
+}
